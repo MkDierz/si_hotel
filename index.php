b5d385c5c879bc2ef2778c59c53ff29d5ee87862
MkDierz <msyibbran24@gmail.com>
2021-06-27 11:55:00 +0700

    update table dengan data mysql

--- index.php
+++ index.php
@@ -15,22 +15,36 @@ print_r($user_data);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotel</title>
 </head>
 
 <body>
-    <table>
+    <table  style="border: 1px solid #999;padding: 0.5rem;">
         <thead>
             <tr>
+                <th>No.</th>
                 <th>Nama Kamar</th>
                 <th>Status</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>January</td>
-                <td>$100</td>
-            </tr>
+            <?php
+            $i = 1;
+            while ($result = mysqli_fetch_array($data)) {
+            ?>
+                <tr>
+                    <td><?php echo $i ?></td>
+                    <td><?php echo $result['kamar_nama'] ?></td>
+                    <td><?php if ($result['kamar_status'] == 1) {
+                        echo "Aviable";
+                    }else {
+                        echo "taken";
+                    } ?></td>
+                </tr>
+            <?php
+                $i++;
+            }
+            ?>
         </tbody>
     </table>
 </body>
